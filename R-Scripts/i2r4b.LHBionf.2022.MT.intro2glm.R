7157dce73b3dbc1590588680b931f1aedbcb56c8
mathornton01 <mathornton@smu.edu>
2022-05-04 16:19:06 -0500

    Worked on Slides+Code for R Nanocourse

--- R-Scripts/i2r4b.LHBionf.2022.MT.intro2glm.R
+++ R-Scripts/i2r4b.LHBionf.2022.MT.intro2glm.R
@@ -189,3 +189,239 @@ iris.form.1 <- formula(Sepal.Length~Sepal.Width*Petal.Length*Petal.Width+Species
 iris.mod.1 <- glm(iris.form.1,data=iris); 
 
 plot(iris.mod.1)
+
+
+
+
+
+
+
+# Suppose you would like to build a linear model for a subset of the data (train)
+#  Then use the other part (test) to determine how well the model fit.  This 
+#  can be done by first creating a selector with the desired proportions 
+
+
+iris.form.1 <- formula(Sepal.Length~Sepal.Width*Petal.Length*Petal.Width+as.factor(Species)); 
+M <- 100000; 
+
+percentage.training <- 0.7; 
+coefs.vec <- list(); 
+for (i in 1:M){
+ind.train <- rbinom(n=nrow(iris),size=1,prob=percentage.training);
+glm(iris.form.1,data=iris[which(as.logical(ind.train)),]) -> iris.mod.1;
+coefs.vec <- append(coefs.vec, list(coef(iris.mod.1)));
+}
+
+unlist(lapply(1:M,function(x){coefs.vec[[x]][1]})) -> intercept.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][2]})) -> SepalWidth.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][3]})) -> PetalLength.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][4]})) -> PetalWidth.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][5]})) -> Versicolor.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][6]})) -> Virginica.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][7]})) -> SW.PL.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][8]})) -> SW.PW.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][9]})) -> PL.PW.estimates;
+unlist(lapply(1:M,function(x){coefs.vec[[x]][10]})) -> SW.PL.PW.estimates;
+
+
+pdf("Simulation-Plot-Results.PDF",width=18,height=8);
+
+par(mfrow=c(1,2));
+
+plot(density(intercept.estimates),
+     main=paste("Intercept in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(intercept.estimates),lty=2,col="blue");
+arrows(x0=mean(intercept.estimates),
+       y0=0.2,
+       x1=mean(intercept.estimates)+sd(intercept.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(intercept.estimates),digits=2)
+simmu <- format(mean(intercept.estimates),digits=2)
+text(x=(2*mean(intercept.estimates)+sd(intercept.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(intercept.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(SepalWidth.estimates),
+     main=paste("Sepal Width Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(SepalWidth.estimates),lty=2,col="blue");
+arrows(x0=mean(SepalWidth.estimates),
+       y0=0.2,
+       x1=mean(SepalWidth.estimates)+sd(SepalWidth.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(SepalWidth.estimates),digits=2)
+simmu <- format(mean(SepalWidth.estimates),digits=2)
+text(x=(2*mean(SepalWidth.estimates)+sd(SepalWidth.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(SepalWidth.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(PetalLength.estimates),
+     main=paste("Petal Length Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(PetalLength.estimates),lty=2,col="blue");
+arrows(x0=mean(PetalLength.estimates),
+       y0=0.2,
+       x1=mean(PetalLength.estimates)+sd(PetalLength.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(PetalLength.estimates),digits=2)
+simmu <- format(mean(PetalLength.estimates),digits=2)
+text(x=(2*mean(PetalLength.estimates)+sd(PetalLength.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(PetalLength.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(PetalWidth.estimates),
+     main=paste("Petal Width Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(PetalWidth.estimates),lty=2,col="blue");
+arrows(x0=mean(PetalWidth.estimates),
+       y0=0.2,
+       x1=mean(PetalWidth.estimates)+sd(PetalWidth.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(PetalWidth.estimates),digits=2)
+simmu <- format(mean(PetalWidth.estimates),digits=2)
+text(x=(2*mean(PetalWidth.estimates)+sd(PetalWidth.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(PetalWidth.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(Versicolor.estimates),
+     main=paste("Versicolor Species Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(Versicolor.estimates),lty=2,col="blue");
+arrows(x0=mean(Versicolor.estimates),
+       y0=0.2,
+       x1=mean(Versicolor.estimates)+sd(Versicolor.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(Versicolor.estimates),digits=2)
+simmu <- format(mean(Versicolor.estimates),digits=2)
+text(x=(2*mean(Versicolor.estimates)+sd(Versicolor.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(Versicolor.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(Virginica.estimates),
+     main=paste("Virginica Species Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(Virginica.estimates),lty=2,col="blue");
+arrows(x0=mean(Virginica.estimates),
+       y0=0.2,
+       x1=mean(Virginica.estimates)+sd(Virginica.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(Virginica.estimates),digits=2)
+simmu <- format(mean(Virginica.estimates),digits=2)
+text(x=(2*mean(Virginica.estimates)+sd(Virginica.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(Virginica.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(SW.PL.estimates),
+     main=paste("Sepal Width & Petal Length Joint Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(SW.PL.estimates),lty=2,col="blue");
+arrows(x0=mean(SW.PL.estimates),
+       y0=0.2,
+       x1=mean(SW.PL.estimates)+sd(SW.PL.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(SW.PL.estimates),digits=2)
+simmu <- format(mean(SW.PL.estimates),digits=2)
+text(x=(2*mean(SW.PL.estimates)+sd(SW.PL.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(SW.PL.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(SW.PW.estimates),
+     main=paste("Sepal Width & Petal Width Joint Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(SW.PW.estimates),lty=2,col="blue");
+arrows(x0=mean(SW.PW.estimates),
+       y0=0.2,
+       x1=mean(SW.PW.estimates)+sd(SW.PW.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(SW.PW.estimates),digits=2)
+simmu <- format(mean(SW.PW.estimates),digits=2)
+text(x=(2*mean(SW.PW.estimates)+sd(SW.PW.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(SW.PW.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(PL.PW.estimates),
+     main=paste("Petal Length & Petal Width Joint Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(PL.PW.estimates),lty=2,col="blue");
+arrows(x0=mean(PL.PW.estimates),
+       y0=0.2,
+       x1=mean(PL.PW.estimates)+sd(PL.PW.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(PL.PW.estimates),digits=2)
+simmu <- format(mean(PL.PW.estimates),digits=2)
+text(x=(2*mean(PL.PW.estimates)+sd(PL.PW.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(PL.PW.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+plot(density(SW.PL.PW.estimates),
+     main=paste("Sepal Width & Petal Length & Petal Width Joint Effect Estimates  in MV LR model for iris data (M = ", M, " Simulations)\n",
+                as.character(iris.form.1)[[2]], " = ", as.character(iris.form.1)[[3]]));
+abline(v=mean(SW.PL.PW.estimates),lty=2,col="blue");
+arrows(x0=mean(SW.PL.PW.estimates),
+       y0=0.2,
+       x1=mean(SW.PL.PW.estimates)+sd(SW.PL.PW.estimates),
+       y1=0.2,
+       code=3,
+       length=0.1)
+simsd <- format(sd(SW.PL.PW.estimates),digits=2)
+simmu <- format(mean(SW.PL.PW.estimates),digits=2)
+text(x=(2*mean(SW.PL.PW.estimates)+sd(SW.PL.PW.estimates))/2,y=0.18,
+     labels=bquote(sigma == .(simsd)))
+text(x=mean(SW.PL.PW.estimates)+.3,y=0.05,labels=bquote(mu == .(simmu)))
+
+dev.off()
+
+
+trainprop <- 0.8; 
+trainnum <- floor(trainprop*nrow(iris));
+testnum <- nrow(iris)-trainnum; 
+trainind <- sample(c(rep(T,trainnum),rep(F,testnum)));
+
+
+iris.lm.pred.form.1 <- formula(Sepal.Length~Sepal.Width+Petal.Length+Petal.Width);
+iris.lm.pred.mod.1 <- glm(iris.lm.pred.form.1,data=iris[trainind,]);
+
+par(mfrow=c(1,1))
+iris.lm.pred.mod.1.predictions <- predict(iris.lm.pred.mod.1,newdata=iris[!trainind,]);
+plot(iris.lm.pred.mod.1.predictions,iris[!trainind,"Sepal.Length"],
+     xlab="Predicted Sepal Length", ylab="Actual Sepal Length",
+     main=paste("True vs. Predicted Sepal Length from Linear Model\n",
+                as.character(iris.lm.pred.form.1)[2], " = ", as.character(iris.lm.pred.form.1)[3]));
+
+Petal.Width.Quantiles <- quantile(iris$Petal.Width,c(0.33,0.66));
+iris$Petal.Width.Class <- ifelse(iris$Petal.Width <= Petal.Width.Quantiles[1], "Low", 
+                                 ifelse(iris$Petal.Width <= Petal.Width.Quantiles[2], "Medium", "High"));
+
+iris$Petal.Width.Class <- ordered(iris$Petal.Width.Class,levels=c("Low","Medium","High"));
+iris$Petal.Width.Class;
+
+iris.form.2 <- formula(Petal.Length~Petal.Width.Class);
+iris.mod.2 <- glm(iris.form.2,data=iris);
+summary(iris.mod.2);
+
+iris$Linear.PWC <- ifelse(iris$Petal.Width <= Petal.Width.Quantiles[1], 0, 
+                                 ifelse(iris$Petal.Width <= Petal.Width.Quantiles[2], 1, 2));
+iris$Quadratic.PWC <- iris$Linear.PWC^2;
+iris.form.3 <- formula(Petal.Length~Linear.PWC+Quadratic.PWC);
+iris.mod.3 <- glm(iris.form.3,data=iris);
+
+summary(iris.mod.3);
